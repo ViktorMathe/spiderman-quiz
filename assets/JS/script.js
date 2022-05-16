eda6e7e3d739ced371de2f60f9e00d39a31def05
ViktorMathe <matheviktor@icloud.com>
2022-05-16 22:13:47 +0000

    Add function to the buttons to go on the next section

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,28 @@
+let main= document.getElementById('startpage');
+let cat= document.getElementById('cat');
+let quiz= document.getElementById('quiz');
+
+function startpage(){
+        main.style.display='block';
+        cat.style.display='none';
+        quiz.style.display='none'
+}
+
+function choice(){
+        main.style.display='none';
+        cat.style.display='block';
+        quiz.style.display='none';
+}
+
+function categories(){
+        main.style.display="none";
+        cat.style.display='none';
+        quiz.style.display='block'
+}
+
+
+
+
 let Questions=[{
     id:0,
     q: "When was the first Spider-Man comic released?",
@@ -7,30 +32,3 @@ let Questions=[{
         {text: "1973",isCorrect:false}]
 }]
 
-var start = true;
-
-function quizGame(id){
-    const question=document.getElementById('question')
-    question.innerText= Questions[id].q;
-
-    const op1=document.getElementById('answer1');
-    const op2=document.getElementById('answer2');
-    const op3=document.getElementById('answer3');
-    const op4=document.getElementById('answer4');
-
-    op1.innerText = Questions[id].a[0].text;
-    op2.innerText = Questions[id].a[1].text;
-    op3.innerText = Questions[id].a[2].text;
-    op4.innerText = Questions[id].a[3].text;
-
-    op1.value = Questions[id].a[0].isCorrect;
-    op2.value = Questions[id].a[1].isCorrect;
-    op3.value = Questions[id].a[2].isCorrect;
-    op4.value = Questions[id].a[3].isCorrect;
-
-    var selected= "";
-
-    if(start){
-        quizGame('0');
-    }
-}
