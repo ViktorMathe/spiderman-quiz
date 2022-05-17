6f41b6ee9840568ec05a1d28797393c67364f68d
ViktorMathe <matheviktor@icloud.com>
2022-05-17 18:34:30 +0000

    add functions to change between the sections'

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,50 +1,53 @@
-let body = document.getElementsByClassName('background');
+let body = document.getElementById('body');
 let main = document.getElementById('startpage');
+let startq = document.getElementById('startq')
 let cat = document.getElementById('cat');
 let quiz = document.getElementById('quiz');
 let welcome = document.getElementById('starting');
-let head = document.getElementsByTagName('header');
+
+welcome.classList.remove('hide');
 
 function startpage() {
-       welcome.style.display='none';
-       main.classList.remove('hide');
-       cat.style.display='none';
-       quiz.style.display='none';
+    welcome.classList.add('hide');
+    body.classList.remove('welcome');
+    main.classList.remove('hide');
+    cat.style.display = 'none';
+    quiz.style.display = 'none';
 }
 
 function choice() {
-        main.style.display = 'none';
-        cat.style.display = 'block';
-        quiz.style.display = 'none';
+    main.classList.remove('hide');
+    startq.style.display = 'none';
+    cat.style.display = 'block';
 }
 
 function categories() {
-        main.style.display = "none";
-        cat.style.display = 'none';
-        quiz.style.display = 'block'
+    main.classList.remove('hide');
+    cat.style.display = "none";
+    quiz.style.display = 'block';
 }
 
 
 
 
-let Questions = [{
-        id: 0,
-        q: "When was the first Spider-Man comic released?",
-        a: [{
-                        text: "1962",
-                        isCorrect: true
-                },
-                {
-                        text: "1958",
-                        isCorrect: false
-                },
-                {
-                        text: "1948",
-                        isCorrect: false
-                },
-                {
-                        text: "1973",
-                        isCorrect: false
-                }
-        ]
+let questions = [{
+
+    question: "When was the first Spider-Man comic released?",
+    answer: [{
+            text: "1962",
+            isCorrect: true
+        },
+        {
+            text: "1958",
+            isCorrect: false
+        },
+        {
+            text: "1948",
+            isCorrect: false
+        },
+        {
+            text: "1973",
+            isCorrect: false
+        }
+    ]
 }]
