e8b25c46a170d120134bafa7b35171e8c1a9f1ad
ViktorMathe <matheviktor@icloud.com>
2022-05-26 22:41:30 +0000

    Restructred the questions and add event listeners to the buttons

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,165 +7,289 @@ let welcome = document.getElementById('starting');
 let scorenumbers = document.getElementById('scores');
 let scorepage = document.getElementById('score-page');
 let timer = document.getElementById('timer');
+let endresult = document.getElementById('end-result');
 welcome.classList.remove('hide');
 
-let currentQuestion;
-let randomQuestions;
+let questionAnswered;
 let questionsSet;
-let questionPool;
-let qanswered;
+let categoryChoosen;
+let currentQuestion;
 let counter;
 
-function navigation(){
-  cat.style.display='none';
-  quiz.style.display='none';
-  scorepage.style.display='none';
+
+function reset() {
+  cat.style.display = 'none';
+  quiz.style.display = 'none';
+  scorepage.style.display = 'none';
+  scorenumbers.style.display = 'none';
+  timer.style.display = 'none';
   clearInterval(counter);
   seconds = 60;
+  scores = 0;
 }
 
-function startpage() {
+function startpage(event) {
   console.log('Start page')
   welcome.classList.add('hide');
   body.classList.remove('welcome');
   main.classList.remove('hide');
-  startq.style.display='block';
-  navigation();
+  startq.style.display = 'block';
+  reset();
 }
 
-function choice() {
-  console.log('Category choices')
+function choice(event) {
+  console.log('Category choices');
   main.classList.remove('hide');
   startq.style.display = 'none';
   scorepage.style.display = 'none';
   cat.style.display = 'block';
 }
 
-function categories(cats) {
+function categories(category) {
   main.classList.remove('hide');
   cat.style.display = "none";
   scorepage.style.display = 'none';
   quiz.style.display = 'block';
+  scorenumbers.style.display = 'block';
+  timer.style.display = 'block';
 
-  catchoice = cats;
+  categoryChoosen = category;
 
   startquiz();
 }
 
+function startquiz(event) {
+  console.log('Game started');
+  questionAnswered = 0;
+  choosecat();
+  randomquestions();
+  showquestions();
+  starttimer();
+  clearInterval(countdown);
+}
 
-let questions = [
-  [
-    ["When was the first Spider-Man comic released?", "1962", "1958", "1948", "1973", "1962"],
-
-    ["Who wrote the most Spider-Man comics?", "Steve Ditko", "Kaare Andrews", "Stan Lee", "Gerard Way", "Stan Lee"],
-
-    ["Who was Spider-Man's strongest enemy?", "Rhino", "Sandman", "Venom", "Doctor Octopus", "Rhino"],
-
-    ["Who is the most popular superhero?", "Iron-Man", "Captain America", "Hulk", "Spider-Man", "Spider-Man"],
-
-    ["Which of these is NOT Peter Parker's special ability?", "Enhanced strength", "Reflexes", "Durability", "Flying", "Flying"]
-  ],
-  [
-    ['Which film starred Tom Holland?', 'Spider-Man', 'The Amazing Spider-Man', 'Spider-Man 3', 'Spider-Man Homecoming', 'Spider-Man Homecoming'],
-
-    ['When was the fist Spider-Man movie released in the cinema?', '2012', '2002', '2004', '2017', '2002'],
-
-    ['Who was the villain in the Amazing Spider-Man 2?', 'Lizard', 'Green Goblin and Electro', 'Doctor Octopus', 'Sandman and Venom', "Green Goblin and Electro"],
-
-    ['How many times has MJ been captured by a villain?', '4', '3', 'None', '6', '4'],
-
-    ["What is MJ's full name in the films that Tom Holland starred in?", 'Mary-Jane Wattson', 'Michelle Jones-Wattson', 'Michelle-Jane Wattson', 'None of these', 'Michelle Jones-Wattson'],
-
-  ],
-  [
-    ['How many Spider-Man games have been released since 2000?', '32', '20', '4', '67', '20'],
-
-    ['Which console has the exclusive right to the most famous Spider-Man game?', 'Microsoft XBOX', 'Nintendo', 'PC', 'PlayStation', 'PlayStation'],
-
-    ['How old is Peter Parker in Spider-Man (2018) video game?', '16', '29', '23', '33', '23'],
-
-    ['What character can you control in the Spider-Man (2018) video game?', 'Aunt May', 'Uncle Ben', 'Otto Octavius', 'MJ', 'MJ'],
-
-    ['Which game has Spider-Man appeared in?', 'Fortnite', 'Call of Duty', 'Battlefield', 'Minecraft', 'Fortnite'],
-  ]
-
-];
-
-function choosecat() {
+function choosecat(event) {
   console.log("Category choosen");
-  const fullQuestions = JSON.parse(JSON.stringify(questions));
-  if (catchoice === 'comic') {
-    questionsSet = fullQuestions[0];
-  } else if (catchoice === 'film') {
-    questionsSet = fullQuestions[1];
-  } else if (catchoice === 'game') {
-    questionsSet = fullQuestions[2];
-  } else {
-    alert("No valid category been chosen");
+  const questionList = [...questions];
+  switch (categoryChoosen) {
+
+    case "comic":
+      questionsSet = questionList[0];
+      break;
+    case "film":
+      questionsSet = questionList[1];
+      break;
+    case "game":
+      questionsSet = questionList[2];
+      break;
+    default:
+      alert("No valid category been chosen");
   }
 }
 
 
+let questions = [
+  [{
+      "question": "When was the first Spider-Man comic released?",
+      "answerone": "1962",
+      "answertwo": "1958",
+      "answerthree": "1948",
+      "answerfour": "1973",
+      "correctanswer": "1962",
+    },
+
+    {
+      "question": "Who wrote the most Spider-Man comics?",
+      "answerone": "Steve Ditko",
+      "answertwo": "Kaare Andrews",
+      "answerthree": "Stan Lee",
+      "answerfour": "Gerard Way",
+      "correctanswer": "Stan Lee"
+    },
+
+    {
+      "question": "Who was Spider-Man's strongest enemy?",
+      "answerone": "Rhino",
+      "answertwo": "Sandman",
+      "answerthree": "Venom",
+      "answerfour": "Doctor Octopus",
+      "correctanswer": "Rhino"
+    },
+
+    {
+      "question": "Who is the most popular superhero?",
+      "answerone": "Iron-Man",
+      "answertwo": "Captain America",
+      "answerthree": "Hulk",
+      "answerfour": "Spider-Man",
+      "correctanswer": "Spider-Man"
+    },
+
+    {
+      "question": "Which of these is NOT Peter Parker's special ability?",
+      "answerone": "Enhanced strength",
+      "answertwo": "Reflexes",
+      "answerthree": "Durability",
+      "answerfour": "Flying",
+      "correctanswer": "Flying"
+    }
+  ],
+
+  [{
+      "question": 'Which film starred Tom Holland?',
+      "answerone": 'Spider-Man',
+      "answertwo": 'The Amazing Spider-Man',
+      "answerthree": 'Spider-Man 3',
+      "answerfour": 'Spider-Man Homecoming',
+      "correctanswer": 'Spider-Man Homecoming'
+    },
+
+    {
+      "question": 'When was the fist Spider-Man movie released in the cinema?',
+      "answerone": '2012',
+      "answertwo": '2002',
+      "answerthree": '2004',
+      "answerfour": '2017',
+      "correctanswer": '2002'
+    },
+
+    {
+      "question": 'Who was the villain in the Amazing Spider-Man 2?',
+      "answerone": 'Lizard',
+      "answertwo": 'Green Goblin and Electro',
+      "answerthree": 'Doctor Octopus',
+      "answerfour": 'Sandman and Venom',
+      "correctanswer": "Green Goblin and Electro"
+    },
+
+    {
+      "question": 'How many times has MJ been captured by a villain?',
+      "answerone": '4',
+      "answertwo": '3',
+      "answerthree": 'None',
+      "answerfour": '6',
+      "correctanswer": '4'
+    },
+
+    {
+      "question": "What is MJ's full name in the films that Tom Holland starred in?",
+      "answerone": 'Mary-Jane Wattson',
+      "answertwo": 'Michelle Jones-Wattson',
+      "answerthree": 'Michelle-Jane Wattson',
+      "answerfour": 'None of these',
+      "correctanswer": 'Michelle Jones-Wattson'
+    }
+  ],
+
+
+  [{
+      "question": 'How many Spider-Man games have been released since 2000?',
+      "answerone": '32',
+      "answertwo": '20',
+      "answerthree": '4',
+      "answerfour": '67',
+      "correctanswer": '20'
+    },
+
+    {
+      "question": 'Which console has the exclusive right to the most famous Spider-Man game?',
+      "answerone": 'Microsoft XBOX',
+      "answertwo": 'Nintendo',
+      "answerthree": 'PC',
+      "answerfour": 'PlayStation',
+      "correctanswer": 'PlayStation'
+    },
+
+    {
+      "question": 'How old is Peter Parker in Spider-Man (2018) video game?',
+      "answerone": '16',
+      "answertwo": '29',
+      "answerthree": '23',
+      "answerfour": '33',
+      "correctanswer": '23'
+    },
+
+    {
+      "question": 'What character can you control in the Spider-Man (2018) video game?',
+      "answerone": 'Aunt May',
+      "answertwo": 'Uncle Ben',
+      "answerthree": 'Otto Octavius',
+      "answerfour": 'MJ',
+      "correctanswer": 'MJ'
+    },
+
+    {
+      "question": 'Which game has Spider-Man appeared in?',
+      "answerone": 'Fortnite',
+      "answertwo": 'Call of Duty',
+      "answerthree": 'Battlefield',
+      "answerfour": 'Minecraft',
+      "correctanswer": 'Fortnite'
+    }
+  ],
+];
+
 
 function randomquestions() {
-  questionPool = questionsSet.length
-  let randomQuestion = Math.floor(Math.random() * questionPool);
+  let randomQuestion = Math.floor(Math.random() * questionsSet.length);
   currentQuestion = questionsSet[`${randomQuestion}`];
 }
 
-
-
-function showquestions(questions) {
+function showquestions() {
 
   const questiontext = document.getElementById('question');
-  questiontext.innerText = currentQuestion[0];
+  questiontext.innerText = currentQuestion.question;
 
   let answerone = document.getElementById('answer1');
-  answerone.innerText = currentQuestion[1];
+  answerone.innerText = currentQuestion.answerone;
 
   const answertwo = document.getElementById('answer2');
-  answertwo.innerText = currentQuestion[2];
+  answertwo.innerText = currentQuestion.answertwo;
 
   const answerthree = document.getElementById('answer3');
-  answerthree.innerText = currentQuestion[3];
+  answerthree.innerText = currentQuestion.answerthree;
 
   const answerfour = document.getElementById('answer4');
-  answerfour.innerText = currentQuestion[4];
+  answerfour.innerText = currentQuestion.answerfour;
+}
+
+function checkanswers(event) {
+  console.log('Check answer');
+  if (this.dataset.answer = currentQuestion.correctanswer) {
+    console.log('Correct Answer');
+    questionAnswered++;
+    scores++;
+    scorepush();
+  } else {
+    console.log('Wrong Answer');
+    questionAnswered++;
+  }
+
+  nextquestion();
 }
 
 function nextquestion() {
   console.log('Next question');
-  if (qanswered < questionPool) {
-    questionPool--;
+  if (questionAnswered < questionsSet.length) {
+    removeAnsweredQ();
     randomquestions();
     showquestions();
-  } else if (qanswered === questionPool) {
+  } else if (questionAnswered === questionsSet.length) {
     gameover();
   } else {
     alert('There was a problem! Please start again');
   }
-  /*showquestions(randomQuestions[currentQuestion]); */
 
 
 }
 
 function removeAnsweredQ() {
   console.log('Remove answered question');
-  let questionshown = questionsSet.currentQuestion;
-  questionsSet.splice(questionshown, 0);
+  let questionshown = questionsSet.indexOf(currentQuestion);
+  questionsSet.splice(questionshown, 1);
 }
 
 
-function startquiz() {
-  console.log('Game started');
-  choosecat();
-  randomquestions();
-  showquestions();
-  starttimer();
-  clearInterval(countdown);
-  qanswered = 0;
-  scores = 0;
-}
 
 function starttimer() {
   timer.innerHTML = '60 seconds';
@@ -186,21 +310,6 @@ function endtimer() {
   gameover();
 }
 
-function checkanswers(num) {
-  console.log('Check answer');
-  if (currentQuestion[num] == currentQuestion[5]) {
-    console.log('Correct Answer');
-    qanswered++;
-    scores++;
-    scorepush();
-  } else {
-    console.log('Wrong Answer');
-    qanswered++;
-  }
-  removeAnsweredQ();
-
-  nextquestion();
-}
 
 function scorepush() {
   scorenumbers.innerHTML = `Score: ${scores}/5`;
@@ -221,4 +330,11 @@ function endscore() {
   scorenumbers.style.display = 'none';
   timer.style.display = 'none';
   scorepage.style.display = 'block';
+  endresult.innerText = `Your result: ${scores}/5`;
 }
+
+
+welcome.addEventListener("click",startpage);
+document.getElementById('home-button').addEventListener('click', startpage);
+startq.addEventListener('click', choice);
+document.getElementById('answers').addEventListener('click',checkanswers);
